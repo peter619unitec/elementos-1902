611198c81fe476dbaf154fb13b69f962166c0604
peter619unitec <43156624+peter619unitec@users.noreply.github.com>
2019-02-15 21:44:30 -0600

    updatedd

---
+++ src/main/java/org/unitec/elementos/Localizacion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos;
+
+/**
+ *
+ * @author Mk-61
+ */
+public class Localizacion {
+    private long lat;
+    private long lon;
+
+    public Localizacion() {
+    }
+
+    public long getLat() {
+        return lat;
+    }
+
+    public void setLat(long lat) {
+        this.lat = lat;
+    }
+
+    public long getLon() {
+        return lon;
+    }
+
+    public void setLon(long lon) {
+        this.lon = lon;
+    }
+    
+}
